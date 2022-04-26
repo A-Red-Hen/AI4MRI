62e4859950df3ae620949f96c70c461e87fd9cb3
Neha <neha.s3k@gmail.com>
2022-04-25 19:01:58 -0700

    Initial Commit

---
+++ AI4MRI/AI4MRIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI4MRIApp.swift
+//  AI4MRI
+//
+//  Created by Neha Shaik on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct AI4MRIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
